1c1f808a7913e3dff6eda1f2c9a95ebfdc0b2baf
developerimran <developerimran.in@gmail.com>
2024-09-21 11:59:53 +0600

    some css and index file work

--- src/main.js
+++ src/main.js
@@ -1,105 +1,162 @@
-const student_create_form = document.getElementById("student-create");
-const studentList = document.getElementById("student-deta-list");
+const studentCreateForm = document.getElementById("student-create-form");
+const studentResultForm = document.getElementById("student-result-form");
+const studentDataList = document.getElementById("student-deta-list");
 const msg = document.querySelector(".msg");
-const singalStudentData = document.querySelector(".student-data");
+const btnClose = document.querySelector(".btn-close");
 
-// Student Show form............
+/**
+ *show all data
+ */
 const getAllStudents = () => {
-  const students = getDataLS("students");
+  const data2 = JSON.parse(localStorage.getItem("students"));
 
-  let dataList = "";
+  let listData = "";
+  if (data2) {
+    data2.map((item, index) => {
+      listData += `
+      
+      <tr>
+       <td><img
+        src="${item.photo}"
+        alt=""/></td>
+      <td>${item.name}</td>
+      <td>${item.roll}</td>
+      <td>${item.reg}</td>
+      <td>${item.class}</td>
+      <td>${item.examination}</td>
+      <td>${item.board}</td>
+      <td>${timeAgo(item.createAt)}</td>
 
-  if (students) {
-    students.forEach((item, index) => {
-      dataList += `<tr>
-                      <td>${index + 1}</td>
-                      <td>
-                        <img src="${item.photo}" alt="" />
-                      </td>
-                      <td>${item.name}</td>
-                      <td>${item.email}</td>
-                      <td>${item.phone}</td>
-                      <td>${item.location}</td>
-                      <td>${timeAgo(item.createdAt)}</td>
-                      <td>
-                        <button
-                          class="btn btn-sm btn-info"
-                          data-bs-toggle="modal"
-                          data-bs-target="#student-show"
-                          onclick="showSingalStudent('${item.id}')"
-                        >
-                          <i class="fa fa-eye"></i>
-                        </button>
-                        <button class="btn btn-sm btn-warning">
-                          <i class="fa fa-edit"></i>
-                        </button>
-                        <button class="btn btn-sm btn-danger" onclick="deleteStudens('${
-                          item.id
-                        }')">
-                          <i class="fa fa-trash"></i>
-                        </button>
-                      </td>
-                    </tr>`;
+
+      <td>
+      ${
+        item.results
+          ? `<button  class="btn btn-sm btn-success">View Result</button>`
+          : `<button  class="btn btn-sm btn-info" data-bs-toggle="modal" data-bs-target="#student-result-form" onclick="addStudentResultModel('${item.id}')">Add Result</button>`
+      }
+      </td>
+
+      <td>
+
+      <button  class="btn btn-sm btn-info"><i class="fa fa-eye" ></i></button>
+      <button  class="btn btn-sm btn-warning"><i class="fa fa-edit" ></i></button>
+      <button  class="btn btn-sm btn-danger"><i class="fa fa-trash" ></i></button>
+      </td>
+      </tr>
+      
+      `;
     });
   } else {
-    dataList = `<tr>
-                    <td colspan="7" class="text-center text-danger" >No Data Found</td>
-               </tr>`;
   }
-
-  studentList.innerHTML = dataList;
-};
-
-const deleteStudens = (id) => {
-  const conf = confirm("Are You suer?");
-
-  if (conf) {
-    deleteSingleData("students", id);
-    getAllStudents();
-  }
-};
-
-const showSingalStudent = (id) => {
-  const { name, email, phone, location, photo } = getSignalData("students", id);
-
-  singalStudentData.innerHTML = `<img
-                src="${photo}"
-                alt=""
-              />
-              <h2>${name}</h2>
-              <p>${email}</p>
-              <p>${phone}</p>
-              <p>${location}</p>
-              `;
+  studentDataList.innerHTML = listData;
 };
 getAllStudents();
-//Now Submit Create student Form........
-student_create_form.onsubmit = (e) => {
-  e.preventDefault();
 
+/**
+ * Submit Student Create Form
+ */
+
+studentCreateForm.onsubmit = (e) => {
+  e.preventDefault();
   const form_data = new FormData(e.target);
-  const { name, email, phone, location, photo } = Object.fromEntries(form_data);
-  if (!name || !email || !phone || !location || !photo) {
-    msg.innerHTML = createAlert("All fields are requard");
-  } else if (!isEmail(email)) {
-    msg.innerHTML = createAlert("Invalid email Address", "warning");
-  } else if (!isMobile(phone)) {
-    msg.innerHTML = createAlert("Invalid Mobile Nubmer", "warning");
+  const data = Object.fromEntries(form_data.entries());
+
+  //form valadation
+
+  if (
+    !data.name ||
+    !data.father ||
+    !data.mother ||
+    !data.dob ||
+    !data.roll ||
+    !data.reg ||
+    !data.class ||
+    !data.examination ||
+    !data.institute ||
+    !data.board ||
+    !data.year ||
+    !data.group ||
+    !data.type
+  ) {
+    msg.innerHTML = createAlert("All Fields are Requerd");
   } else {
-    sendDataLS("students", {
+    let oldData = [];
+
+    //check Old data localstorage or not
+
+    if (localStorage.getItem("students")) {
+      oldData = JSON.parse(localStorage.getItem("students"));
+    }
+    oldData.push({
+      ...data,
       id: createID(),
-      name,
-      email,
-      phone,
-      location,
-      photo,
-      createdAt: Date.now(),
+      createAt: Date.now(),
+      updatedAt: null,
+      results: null,
     });
 
-    msg.innerHTML = createAlert("Student Data Created", "success");
-
+    //send data to LS
+    localStorage.setItem("students", JSON.stringify(oldData));
     e.target.reset();
-
+    btnClose.click();
     getAllStudents();
   }
+};
+
+const addStudentResultModel = (id) => {
+  studentResultForm.querySelector('input[name="id"]').value = id;
+};
+
+/**
+ * Students Result Form
+ * @param {*} e
+ */
+studentResultForm.onsubmit = (e) => {
+  e.preventDefault();
+
+  //get result_form_data
+  const result_form_data = new FormData(e.target);
+  const data = Object.fromEntries(result_form_data.entries());
+
+  //form valadtion
+
+  if (
+    !data.bangla ||
+    !data.english ||
+    !data.mathematics ||
+    !data.socil_Science ||
+    !data.science ||
+    !data.religion
+  ) {
+    msg.innerHTML = createAlert("All Fields are Requerd");
+  }
+
+  //get All students
+  const students = JSON.parse(localStorage.getItem("students"));
+
+  //add Result
+
+  const updateData = students.map((item) => {
+    if (item.id == data.id) {
+      return {
+        ...item,
+        results: {
+          bangla: data.bangla,
+          english: data.english,
+          mathematics: data.mathematics,
+          socil_Science: data.socil_Science,
+          science: data.science,
+          religion: data.religion,
+        },
+      };
+    } else {
+      return item;
+    }
+  });
+
+  localStorage.setItem("students", JSON.stringify(updateData));
+
+  e.target.reset();
+  btnClose.click();
+  getAllStudents();
 };
